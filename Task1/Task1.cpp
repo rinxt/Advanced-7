3092e58aa1bc62115e525b95ef53a897c871547c
Мамаева Арина Евгеньевна <m18282@kometadom.local>
2023-09-19 16:55:21 +0300

    Task1 added

---
+++ Task1/Task1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void print_vec(std::vector<int> v) {
+    std::vector<int>::const_iterator i = v.cbegin();
+    while (i != v.cend())
+    {
+        std::cout << *i << " ";
+        i++;
+    }
+}
+
+int main()
+{
+    std::vector <int> numbers = { 1, 1, 2, 5, 6, 1, 2, 4 };
+
+    std::cout << "[IN]: ";
+    print_vec(numbers);
+
+    std::sort(numbers.begin(), numbers.end(),[](const int& a, const int& b) {
+            return a < b;
+        });
+
+    auto it = std::unique(numbers.begin(), numbers.end());
+    numbers.erase(it, numbers.end());
+
+    std::cout << "\n[OUT]: ";
+    print_vec(numbers);
+}
